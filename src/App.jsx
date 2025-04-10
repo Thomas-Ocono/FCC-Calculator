002f0b678fb7c84d0d33d687baaaabace4687f4a
Thomas O'Connor <thomas.Ocono@gmail.com>
2025-04-10 08:58:00 -0400

    redone html and css, looks nicer

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,113 @@
 import { useState } from "react";
-import CalcButton from "./CalcButton";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div id="container-wrapper">
+      <div id="calc-wrapper">
         <h1 id="title">Calculator-Inator</h1>
-        <h1 id="display">Display</h1>
+        <div id="display-wrapper">
+          <h2 id="display-prev">Prev Value</h2>
+          <h2 id="display">Value</h2>
+        </div>
         <div id="buttons-wrapper">
-          <div id="row-1">
-            <button id="one">1</button>
-            <button id="two">2</button>
-            <button id="three">3</button>
-            <button id="add">+</button>
-          </div>
-          <div id="row-2">
-            <button id="four">4</button>
-            <button id="five">5</button>
-            <button id="six">6</button>
-            <button id="subtract">-</button>
-          </div>
-          <div id="row-3">
-            <button id="seven">7</button>
-            <button id="eight">8</button>
-            <button id="nine">9</button>
-            <button id="multiply">*</button>
-          </div>
-          <div id="row-4">
-            <button id="decimal">.</button>
-            <button id="clear">CE</button>
-            <button id="divide">/</button>
-          </div>
+          <table id="buttons-table">
+            <tr id="row-1">
+              <td>
+                <button className="clear-button" id="clear">
+                  C
+                </button>
+              </td>
+              <td>
+                <button className="operation-button" id="divide">
+                  /
+                </button>
+              </td>
+              <td>
+                <button className="operation-button" id="multiply">
+                  *
+                </button>
+              </td>
+              <td>
+                <button className="operation-button" id="subtract">
+                  -
+                </button>
+              </td>
+            </tr>
+            <tr id="row-2">
+              <td>
+                <button className="number-button" id="seven">
+                  7
+                </button>
+              </td>
+              <td>
+                <button className="number-button" id="eight">
+                  8
+                </button>
+              </td>
+              <td>
+                <button className="number-button" id="nine">
+                  9
+                </button>
+              </td>
+              <td rowSpan={2}>
+                <button className="operation-button" id="add">
+                  +
+                </button>
+              </td>
+            </tr>
+            <tr id="row-3">
+              <td>
+                <button className="number-button" id="four">
+                  4
+                </button>
+              </td>
+              <td>
+                <button className="number-button" id="five">
+                  5
+                </button>
+              </td>
+              <td>
+                <button className="number-button" id="six">
+                  6
+                </button>
+              </td>
+            </tr>
+            <tr id="row-3">
+              <td>
+                <button className="number-button" id="one">
+                  1
+                </button>
+              </td>
+              <td>
+                <button className="number-button" id="two">
+                  2
+                </button>
+              </td>
+              <td>
+                <button className="number-button" id="three">
+                  3
+                </button>
+              </td>
+              <td rowSpan={2}>
+                <button className="operation-button" id="equals">
+                  =
+                </button>
+              </td>
+            </tr>
+            <tr id="row-5">
+              <td colSpan={2}>
+                <button className="number-button" id="zero">
+                  0
+                </button>
+              </td>
+              <td>
+                <button className="number-button" id="decimal">
+                  .
+                </button>
+              </td>
+            </tr>
+          </table>
         </div>
       </div>
     </>
